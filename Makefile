cf01c5c1a36d3cd3cc1d61d938b7028f5d06c1d0
fishy15 <aaryan.prakash3.14@gmail.com>
2020-07-20 09:54:41 -0700

    add banzhaf tests

--- Makefile
+++ Makefile
@@ -10,9 +10,15 @@ banzhaf.o: banzhaf.h banzhaf.cpp
 	$(CC) $(CFLAGS) -c banzhaf.cpp
 
 # tests
+tests: tests/banzhaf_test.out
+
 tests/banzhaf_test.out: fraction.o banzhaf.o tests/banzhaf_test.o
 	$(CC) $(CFLAGS) fraction.o banzhaf.o tests/banzhaf_test.o -o tests/banzhaf_test.out
 
-tests/banzhaf_test.o:
+tests/banzhaf_test.o: tests/banzhaf_test.cpp
 	$(CC) $(CFLAGS) -c tests/banzhaf_test.cpp -o tests/banzhaf_test.o
 
+# clean
+clean:
+	find . -type f -name '*.o' -delete
+	find . -type f -name '*.out' -delete
--- tests/banzhaf_test.cpp
+++ tests/banzhaf_test.cpp
@@ -6,9 +6,13 @@
 
 void print_vector(const std::vector<fraction> &v);
 void test_three_equal();
+void test_four_diff();
+void dictator();
 
 int main() {
     test_three_equal();
+    test_four_diff();
+    dictator();
 }
 
 void print_vector(const std::vector<fraction> &v) {
@@ -33,5 +37,41 @@ void test_three_equal() {
     auto res = banzhaf::calc_index(to_win, weights);
     std::cout << "Power Index: ";
     print_vector(res);
+
+    std::vector<fraction> correct = {fraction(1, 3), fraction(1, 3), fraction(1, 3)};
+    std::cout << "Correct: ";
+    print_vector(correct);
+    std::cout << std::endl;
 }
 
+void test_four_diff() {
+    fraction to_win(7, 1);
+    std::vector<fraction> weights = {fraction::ONE, fraction(2, 1), fraction(3, 1), fraction(4, 1)};
+    std::cout << "Weights: ";
+    print_vector(weights);
+
+    auto res = banzhaf::calc_index(to_win, weights);
+    std::cout << "Power Index: ";
+    print_vector(res);
+
+    std::vector<fraction> correct = {fraction(1, 10), fraction(1, 10), fraction(3, 10), fraction(1, 2)};
+    std::cout << "Correct: ";
+    print_vector(correct);
+    std::cout << std::endl;
+}
+
+void dictator() {
+    fraction to_win(10, 1);
+    std::vector<fraction> weights = {fraction(10, 1), fraction(2, 1), fraction(3, 1), fraction(4, 1)};
+    std::cout << "Weights: ";
+    print_vector(weights);
+
+    auto res = banzhaf::calc_index(to_win, weights);
+    std::cout << "Power Index: ";
+    print_vector(res);
+
+    std::vector<fraction> correct = {fraction::ONE, fraction::ZERO, fraction::ZERO, fraction::ZERO};
+    std::cout << "Correct: ";
+    print_vector(correct);
+    std::cout << std::endl;
+}
